49f8976ade8b7c375f356f3da1c4632d9d152a09
Amar <abcd@gmail.com>
2023-12-31 22:59:11 +0530

    adding homepage script

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import ReactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/smartserv-task-2",
   plugins: [react(), ReactRefresh()],
 });
